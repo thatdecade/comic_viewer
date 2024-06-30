e5a6204a77e1cedf2fcfa36f1ec7e1ac4025965b
Dustin Westaby <thatdecade@gmail.com>
2024-06-29 20:37:43 -0500

    Added user login, auto-assigned admin user and password.

--- comic_web.py
+++ comic_web.py
@@ -1,9 +1,12 @@
-#comic_web.py
-from flask import Flask, render_template, request, jsonify, send_file, redirect, url_for
+from flask import Flask, render_template, request, jsonify, send_file, redirect, url_for, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
 import os
 from datetime import datetime
 import threading
 import requests
+import secrets
 
 from comic_app.settings_manager import SettingsManager, SETTINGS_FILE
 from comic_app.comic_manager import ComicManager
@@ -24,10 +27,58 @@ comic_manager = ComicManager(settings["comics"], lambda: None)
 # Global variable to hold image loading status
 image_status = {"status": "waiting", "file_path": ""}
 
+if "SECRET_KEY" in settings:
+    app.config['SECRET_KEY'] = settings["SECRET_KEY"]
+else:
+    app.config['SECRET_KEY'] = secrets.token_hex(16)
+    settings["SECRET_KEY"] = app.config['SECRET_KEY']
+    settings_manager.save_settings(settings)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+bcrypt = Bcrypt(app)
+
+# User model for the database
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+# Initialize the database and create an admin user if necessary
+with app.app_context():
+    db.create_all()
+    admin_username = 'admin'
+    if not User.query.first():
+        admin_password = secrets.token_urlsafe(16)
+        hashed_password = bcrypt.generate_password_hash(admin_password).decode('utf-8')
+        admin_user = User(username=admin_username, password=hashed_password)
+        db.session.add(admin_user)
+        db.session.commit()
+        settings["admin_password"] = admin_password
+        settings_manager.save_settings(settings)
+    else:
+        admin_user = User.query.first()
+        if "admin_password" not in settings:
+            admin_password = secrets.token_urlsafe(16)
+            hashed_password = bcrypt.generate_password_hash(admin_password).decode('utf-8')
+            admin_user.password = hashed_password
+            db.session.commit()
+            settings["admin_password"] = admin_password
+            settings_manager.save_settings(settings)
+        else:
+            admin_password = settings["admin_password"]
+        
+        # Check if the hash of the stored password matches the hash in the database
+        if bcrypt.check_password_hash(admin_user.password, admin_password):
+            print(f'Username: {admin_username}, Password: {admin_password}')
+            print(f'Please change the default password!')
+
 def download_and_save_comic_image(date_str, folder_path, file_path_jpg):
     global image_status
     if parser_available:
-        print("Downloading comic image...")
+        #print("Downloading comic image...")
         image_status["status"] = "Downloading comic image..."
         comic_name = comic_manager.selected_comic["url"]
         parser = Image_URL_Parser(comic_name)
@@ -37,7 +88,7 @@ def download_and_save_comic_image(date_str, folder_path, file_path_jpg):
             date_str[4:]              # day
         )
         if image_url:
-            print("Parse success, fetch image to save.")
+            #print("Parse success, fetch image to save.")
             fetch_image(image_url, file_path_jpg)
             return
     image_status["status"] = "failure"
@@ -72,18 +123,65 @@ def load_comic_image(selected_comic, selected_date, folder_path):
     file_path_bmp = os.path.join(folder_path, f"{comic_manager.selected_comic['short_code']}{date_str}.bmp")
 
     if os.path.exists(file_path_jpg):
-        print(f"Path exists: {file_path_jpg}")
+        #print(f"Path exists: {file_path_jpg}")
         image_status["file_path"] = file_path_jpg
         image_status["status"] = "success"  # f"Loaded comic from {file_path_jpg}"
     elif os.path.exists(file_path_bmp):
-        print(f"Path exists: {file_path_bmp}")
+        #print(f"Path exists: {file_path_bmp}")
         image_status["file_path"] = file_path_bmp
         image_status["status"] = "success"  # f"Loaded comic from {file_path_bmp}"
     else:
-        print(f"No image found, attempt to download")
+        #print(f"No image found, attempt to download")
         download_and_save_comic_image(date_str, folder_path, file_path_jpg)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('comic_viewer'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/change_password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    if request.method == 'POST':
+        current_password = request.form['current_password']
+        new_password = request.form['new_password']
+        confirm_password = request.form['confirm_password']
+        
+        if not bcrypt.check_password_hash(current_user.password, current_password):
+            flash('Current password is incorrect.', 'danger')
+        elif new_password != confirm_password:
+            flash('New passwords do not match.', 'danger')
+        else:
+            hashed_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
+            current_user.password = hashed_password
+            db.session.commit()
+            flash('Password changed successfully.', 'success')
+            return redirect(url_for('comic_viewer'))
+    
+    return render_template('change_password.html')
+
 @app.route('/', methods=['GET', 'POST'])
+@login_required
 def comic_viewer():
     if request.method == 'POST':
         settings_manager.save_settings(settings)
@@ -92,6 +190,7 @@ def comic_viewer():
     return render_template('index.html', comics=comic_manager.comics)
 
 @app.route('/request_image', methods=['POST'])
+@login_required
 def request_image():
     global image_status
     image_status["status"] = "waiting"
@@ -109,6 +208,7 @@ def request_image():
     return jsonify({"status": "loading"}), 202
 
 @app.route('/status', methods=['GET'])
+@login_required
 def status():
     global image_status
     if image_status["status"] == "success":
@@ -116,9 +216,10 @@ def status():
     return jsonify({"status": image_status["status"]})
 
 @app.route('/image/<path:filename>', methods=['GET'])
+@login_required
 def serve_image(filename):
     folder_path = settings.get("folder_path", os.path.join(os.path.expanduser("~"), "Pictures", "comics"))
-    print(f"Sending {os.path.join(folder_path, filename)}")
+    #print(f"Sending {os.path.join(folder_path, filename)}")
     return send_file(os.path.join(folder_path, filename))
 
 if __name__ == '__main__':
